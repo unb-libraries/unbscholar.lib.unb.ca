d56f614783c93083947998b624cad1418b9d0f23
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2023-02-16 10:12:54 -0400

    SCHOLAR-26 Attempt to customize recent-item-list app

---
+++ build/src/themes/custom/app/home-page/recent-item-list/recent-item-list.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RecentItemListComponent as BaseComponent } from '../../../../app/home-page/recent-item-list/recent-item-list.component';
+
+@Component({
+    selector: 'ds-recent-item-list',
+    templateUrl: './recent-item-list.component.html',
+    styleUrls: ['./recent-item-list.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    animations: [
+        fadeIn,
+        fadeInOut
+    ]
+})
+export class RecentItemListComponent extends BaseComponent {
+
+}
