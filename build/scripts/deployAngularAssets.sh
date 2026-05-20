57062c3cf003c7b6c42d10d892b399f29ef371ad
Jacob Sanford <jsanford@unb.ca>
2026-05-20 10:15:06 -0300

    Favicon fixes to avoid 404s

--- build/scripts/deployAngularAssets.sh
+++ build/scripts/deployAngularAssets.sh
@@ -7,3 +7,6 @@ rsync -a --exclude='themes/custom' /build/src/ /app/src/
 
 # Patch angular.json to add unbscholar theme stylesheet bundle
 sed -i 's|"bundleName": "custom-theme"|"bundleName": "custom-theme" }, { "input": "src/themes/unbscholar/styles/theme.scss", "inject": false, "bundleName": "unbscholar-theme"|' /app/angular.json
+
+# Patch angular.json to serve src/favicon.ico at URL root /favicon.ico (Chrome auto-fetches this regardless of <link rel=icon>)
+sed -i 's|"src/assets",|{ "glob": "favicon.ico", "input": "src/", "output": "/" }, "src/assets",|' /app/angular.json
